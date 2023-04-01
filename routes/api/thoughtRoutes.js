bb085e22548231ffef1bcb8f3d088007faf33247
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-04-01 19:44:04 +0800

    layout of thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getSingleThought,
+    createNewThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController')
+
+// api/thoughts
+router.route('/').get(getAllThoughts).post(createNewThought);
+
+// api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
